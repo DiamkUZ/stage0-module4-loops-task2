0c169c905665103494e7705acc94cbdcbaeada19
DiamkUZ <diamkms@yandex.ru>
2023-02-06 19:43:24 +0300

    all done

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int counter = 0;
+        while (counter <= printToInclusive) {
+            System.out.println(getFactorial(counter));
+            counter++;
+        }
 
+    }
+    public static int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 2;
+        while (counter <= printToInclusive) {
+            boolean isPrimal = true;
+            int testCounter = 2;
+            while (testCounter < counter) {
+                if (counter % testCounter == 0) {
+                    isPrimal = false;
+                }
+                testCounter++;
+            }
+            if (isPrimal ) {
+                System.out.println(counter);
+            }
+            counter++;
+        }
     }
 }
